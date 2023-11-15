ba32f02b0b12aff1032130c303ccf0e6017afced
Shikko01 <ITProgramming070222@gmail.com>
2023-11-15 19:55:27 +0200

    Added LeaveTypeVM Model.

---
+++ LeaveManagement.Web.NET6/Models/LeaveTypeVM.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LeaveManagement.Web.NET6.Models
+{
+    public class LeaveTypeVM
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        [Display(Name = "Default number of days")]
+        public int DefaultDays { get; set; }
+    }
+}
