50738cf67cc4b3e224eb525515f9852d96ab58e3
Shikko01 <ITProgramming070222@gmail.com>
2023-11-17 03:31:37 +0200

    Added GenericRepository<T> class that implements all the methods from IGenericRepository<T> interface.

---
+++ LeaveManagement.Web.NET6/Repositories/GenericRepository.cs
@@ -0,0 +1,52 @@
+﻿using LeaveManagement.Web.NET6.Contracts;
+using LeaveManagement.Web.NET6.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace LeaveManagement.Web.NET6.Repositories
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _context;
+
+        public GenericRepository(ApplicationDbContext context)
+        {
+            this._context = context;
+        }
+
+        public async Task<T> AddAsync(T entity)
+        {
+            await _context.AddAsync(entity);
+            await _context.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var entity = await GetAsync(id);
+            _context.Set<T>().Remove(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<bool> ExistsAsync(int id)
+        {
+            var entity = await GetAsync(id);
+            return entity != null;
+        }
+
+        public async Task<List<T>> GetAllAsync()
+        {
+            return await _context.Set<T>().ToListAsync();
+        }
+
+        public async Task<T> GetAsync(int id)
+        {
+            return await _context.Set<T>().FindAsync(id);
+        }
+
+        public Task UpdateAsync(T entity)
+        {
+            _context.Update(entity);
+            return _context.SaveChangesAsync();
+        }
+    }
+}
