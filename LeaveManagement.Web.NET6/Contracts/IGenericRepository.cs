d843c227530aff9264bfcbb4c514f669ed85b09f
Shikko01 <ITProgramming070222@gmail.com>
2023-11-17 02:54:04 +0200

    Created folder Contracts and added interface IGenericRepository

---
+++ LeaveManagement.Web.NET6/Contracts/IGenericRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace LeaveManagement.Web.NET6.Contracts
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<T> GetAsync(int id);
+        Task<List<T>> GetAllAsync();
+        Task<T> AddAsync(T entity);
+        Task<bool> ExistsAsync(int id);
+        Task DeleteAsync(int id);
+        Task UpdateAsync(T entity);
+    }
+}
